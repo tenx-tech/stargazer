de14c5b76fe7de13ff689e6fd0ee4ad97134f137
Sean Smith <sean.smith@tenx.tech>
2019-04-22 15:22:11 +0800

    Update bundle command for client

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cd node_modules/react-native-stargazer/client && npm run bundle
+cd node_modules/react-native-stargazer/client && npm run build
+mkdir -p ../../../stargazer-ui
+cp -a build/. ../../../stargazer-ui
+
+echo "Stargazer UI succesfully built!!!"
+echo ""
+echo "You should find a new folder in your directory called stargazer-ui which contains the generated UI browser."
+echo ""
