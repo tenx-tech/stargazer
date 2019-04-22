cba4690fb217883dd19cfa24b12fbde86e4fde60
Sean Smith <sean.smith@tenx.tech>
2019-04-22 17:26:59 +0800

    Add webpack for server

---
+++ server/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './server.ts',
+  target: "node",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build')
+  },
+};
