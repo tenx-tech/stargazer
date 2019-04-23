ee7244b05cac24b7b6f9ebd582c8244ea063a777
Sean Smith <sean.smith@tenx.tech>
2019-04-23 12:05:42 +0800

    Fix server run command

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
-    filename: 'index.js',
+    filename: 'server.js',
     path: path.resolve(__dirname, 'build')
   },
 };
