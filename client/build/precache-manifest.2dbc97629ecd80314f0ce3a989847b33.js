f037b36e64996a381a6db0fc57d3a41f1552607a
Sean Smith <sean.smith@tenx.tech>
2019-04-23 23:09:16 +0800

    Check in build output and change screenshots save target

---
+++ client/build/precache-manifest.2dbc97629ecd80314f0ce3a989847b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "318b87d0dc383393874781da9cfdd65f",
+    "url": "./static/media/tenx_logo.318b87d0.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e69ff1807045a1308a63",
+    "url": "./static/js/main.6f3225ef.chunk.js"
+  },
+  {
+    "revision": "f9ba8d874920d0f67d2d",
+    "url": "./static/js/2.518085a5.chunk.js"
+  },
+  {
+    "revision": "e69ff1807045a1308a63",
+    "url": "./static/css/main.838b0e69.chunk.css"
+  },
+  {
+    "revision": "eb5eb78983b955943450c9ed77bd1c4e",
+    "url": "./index.html"
+  }
+];
