d242a319ce9444e14ee13b56e36165d675637b33
Sean Smith <sean.smith@tenx.tech>
2019-04-24 18:33:11 +0800

    Rebuild stargaer-ui output

---
+++ example/stargazer-ui/precache-manifest.e64acfa35fa95fa81d4d01cdf859c8d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "318b87d0dc383393874781da9cfdd65f",
+    "url": "./static/media/tenx_logo.318b87d0.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "301fb37393ed1690b361",
+    "url": "./static/js/main.82d89a0d.chunk.js"
+  },
+  {
+    "revision": "9032de355f18f4d8f51d",
+    "url": "./static/js/2.fac019e5.chunk.js"
+  },
+  {
+    "revision": "301fb37393ed1690b361",
+    "url": "./static/css/main.838b0e69.chunk.css"
+  },
+  {
+    "revision": "8c9d82aa4747e6fdc3469dfdb5d620a0",
+    "url": "./index.html"
+  }
+];
