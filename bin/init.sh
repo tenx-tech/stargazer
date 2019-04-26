40b1768a0f37c0c13fe57fb3c568d7834c8919bb
Sean Smith <sean.smith@tenx.tech>
2019-04-26 09:53:37 +0800

    Add TODO note about alternative built script

--- bin/init.sh
+++ bin/init.sh
@@ -2,6 +2,22 @@
 
 cd node_modules/react-native-stargazer/client
 
+#
+# NOTE: This script copies over the entire pre-built output folder to
+# the user's project directory. This is fast and they don't have to install
+# any dependencies or run a build. However, it's possible they would want
+# to customize this UI output app. If so, an alternative feature could be
+# added which would copy the entire client/ folder (excluding the build output)
+# to the user's root project folder. Then the user could modify that source
+# code and run the build directly there. They would even manage the project
+# dependencies locally in their project and could add other dependencies to
+# customize it if they wished. Adding this feature would just require adding
+# a new build script and command to run it and some new documentation. The
+# script would just copy the client/ folder to the user's root project directory.
+#
+# TODO: Add this feature.
+#
+
 # Create tmp folder to store any existing screenshot files
 mkdir tmp
 
